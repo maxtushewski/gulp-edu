f7965c59d1a0c54828b9550a291269aa60042746
Maks Tushevskiy <max@m-tour.info>
2018-02-26 20:31:28 +0300

    Gulp second commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    minifyCSS = require('gulp-minify-css');
 
 gulp.task('default', function(){
     console.log('Helllo, gulp!');
@@ -25,4 +26,12 @@ gulp.task('sass', function () {
 
 gulp.task('watch', ['browserSync', 'sass'], function () {
     gulp.watch('app/scss/**/*.+(scss|sass)', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('minify', function () {
+    return gulp.src('app/css/**/*.css')
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('dist'));
 });
